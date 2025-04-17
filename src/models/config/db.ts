9d25f7003b79a8a9b647ecdc3424b3d0abd02019
AAndersonSantos <andersonsantos.inc@gmail.com>
2025-04-17 15:23:36 -0300

    Update db.ts

--- src/models/config/db.ts
+++ src/models/config/db.ts
@@ -1,10 +1,13 @@
 import mysql from 'mysql2/promise';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const pool = mysql.createPool({
   host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'chat_app',
+  database: process.env.DATABASE,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   waitForConnections: true,
   connectionLimit: 10
 });
